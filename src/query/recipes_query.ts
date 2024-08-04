fb0a9139fa626d42e98b7aaa24bf469652e063b2
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-08-04 15:15:31 -0400

    adjusts for recipe table and connection to database

--- src/query/recipes_query.ts
+++ src/query/recipes_query.ts
@@ -1,5 +1,5 @@
 export const QUERY =  {
-  SELECT_RECIPES: "SELECT * FROM recipes ORDER BY created_at DESC LIMIT 50",
+  SELECT_RECIPES: "SELECT * FROM recipes LIMIT 50",
   SELECT_RECIPE: "SELECT * FROM recipes WHERE id = ?",
   CREATE_RECIPE: "INSERT INTO recipes (name, cooking_time, steps) VALUES (?, ?, ?)",
   UPDATE_RECIPE: "UPDATE recipes SET name = ?, cooking_time = ?, steps = ?",
