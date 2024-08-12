24fed44729fcf05803a039dd570ab1dde24c1fe2
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-08-11 20:54:27 -0400

    adds validate path to controllers as well as a validate auth function in order to only allow logged in users to get their own recipes

--- src/routes/recipes_routes.ts
+++ src/routes/recipes_routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 import { getRecipes, updateRecipe, createRecipe, deleteRecipe, getRecipe } from '../controller/recipes_controller';
+import { validate_auth } from "../auth/validate_auth";
 
 const recipeRoutes = Router();
 
+// routes for getting recipes specific to users
 recipeRoutes.route("/recipes")
-  .get(getRecipes)
-  .post(createRecipe);
+  .get(validate_auth, getRecipes)
+  .post(validate_auth, createRecipe);
 
 recipeRoutes.route("/recipes/:recipeId")
-  .get(getRecipe)
-  .put(updateRecipe)
-  .delete(deleteRecipe)
+  .get(validate_auth, getRecipe)
+  .put(validate_auth ,updateRecipe)
+  .delete(validate_auth, deleteRecipe)
+
+// routes for getting recipes for all users
+recipeRoutes.route("/recipes/show")
 
 export default recipeRoutes;
